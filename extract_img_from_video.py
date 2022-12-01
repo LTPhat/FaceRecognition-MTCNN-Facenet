b13840fbe3c9c3b512737755b83886917d458fd1
LTPhat <102889533+LTPhat@users.noreply.github.com>
2022-12-01 14:41:24 +0700

    extract img from video

---
+++ extract_img_from_video.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import os
+
+def extract_frame():
+    name_list = ['Duc', 'HDuc', 'Hieu', 'Hung', 'Kien', 'Linh', 'Quan', 'Tan', 'Thang'
+            ,'Truong', 'Tuan', 'Van', 'VietDuc','XuanAnh']
+    root_url = "D:\MTCNN\Video" 
+    img_folder = "D:\MTCNN\Raw_images\\"
+    for name in name_list:
+        video = root_url+ "\\" + name + ".mp4"
+        vidcap = cv2.VideoCapture(video)
+        if os.path.exists(img_folder + name) == False:
+            os.mkdir(img_folder + name)
+        saved_url = img_folder + name
+        count = 50
+        leaf = 0
+        while vidcap.isOpened:
+            success, image = vidcap.read()
+            if success and leaf % 3 == 0:
+                cv2.imwrite(saved_url +"\\{}{}.png".format(name, count), image)
+                count -=1
+            leaf += 1
+            if count < 0:
+                break
